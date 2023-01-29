9a16140bb9e7480036261600f58454bd7115b04f
denis <dublin87@mail.ru>
2023-01-30 01:37:58 +0300

    add callback function on replenishment balance

--- telegram.py
+++ telegram.py
@@ -14,28 +14,13 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 # Включаем логирование
-
 logging.basicConfig(level=logging.INFO)
 botlogfile = open('INFO_log.log')
 botlogfile = open('Eror_log.log')
 logging.basicConfig(level=logging.INFO,filename="INFO_log.log",filemode="w")
 logging.basicConfig(level=logging.ERROR,filename="Eror_log.log",filemode="w")
 
-'''
-@dp.message_handler()
-async def loggin(message: types.Message):
-    dtn = datetime.datetime.now()S
-    botlogfile = open('TestBot.log', 'a')
-    tr= print(dtn.strftime("%d-%m-%Y %H:%M"), 'Пользователь ' + message.from_user.first_name, message.from_user.id, 'написал следующее: ' + message.text, file=botlogfile)
-    botlogfile.close()
-    await message.reply(tr)
-'''
 #Хэндлер — асинхронная функция, которая получает от диспетчера/роутера очередной апдейт и обрабатывает его.
-
-#class Form(StatesGroup):
-#     count= State() 
-
-
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     
@@ -43,16 +28,7 @@ async def process_start_command(message: types.Message):
     keyboard = types.InlineKeyboardMarkup().add(*kb)
     #await Form.count.set()
     await message.reply(f'Привет, {message.from_user.first_name}, \nЯ - бот для пополнения баланса.\nНажмите на кнопку, чтобы пополнить баланс. \nСнизу инлайн кнопка с текстом  Пополнить баланс',reply_markup=keyboard)
-   
-'''
-count=0
-def enter_count():
-    
-    count=int(input('введие сумму:'))
-    return count
-'''
-
-     
+        
 @dp.callback_query_handler(lambda c: c.data == 'press')
 async def EnterSumm(callback_query_handler: types.CallbackQuery):
     #count=int(input('введие сумму:'))
@@ -62,8 +38,6 @@ async def EnterSumm(callback_query_handler: types.CallbackQuery):
 @dp.message_handler(content_types="text")
 async def Vvod(message: types.Message):
     await message.reply(f"Вы хотите положить на счет {message.text} рублей")
-
-
 
 
 @dp.message_handler(commands=['admin'])
@@ -82,13 +56,6 @@ async def process_start_command(message: types.Message):
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
